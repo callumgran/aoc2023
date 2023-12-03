d3c480058ff002f19911169debdb5e9461f603c8
callumgran <callum.gran@gmail.com>
2023-12-03 10:42:10 +0100

    day 3***

--- day3/task_1.c
+++ day3/task_1.c
@@ -8,27 +8,27 @@
 #include "../common.h"
 
 struct number_t {
-	i32 num;
-	i32 start;
-	i32 end;
-	i32 line;
+    i32 num;
+    i32 start;
+    i32 end;
+    i32 line;
 };
 
 struct symbol_t {
-	u8 sym;
-	i32 pos;
-	i32 line;
+    u8 sym;
+    i32 pos;
+    i32 line;
 };
 
 int main(void)
 {
     FILE *fp = fopen("./day3/input.txt", "r");
-	VECTOR(struct number_t, num);
-	VECTOR(struct symbol_t, sym);
-	struct num_vector_t numbers;
-	struct sym_vector_t symbols;
-	vector_init(&numbers, HR_GLOBAL_ALLOCATOR, 10000, NULL);
-	vector_init(&symbols, HR_GLOBAL_ALLOCATOR, 10000, NULL);
+    VECTOR(struct number_t, num);
+    VECTOR(struct symbol_t, sym);
+    struct num_vector_t numbers;
+    struct sym_vector_t symbols;
+    vector_init(&numbers, HR_GLOBAL_ALLOCATOR, 10000, NULL);
+    vector_init(&symbols, HR_GLOBAL_ALLOCATOR, 10000, NULL);
 
     if (fp == NULL) {
         fprintf(stderr, "Error opening file. \n");
@@ -37,53 +37,54 @@ int main(void)
 
     char *line = NULL;
     size_t len = 0;
-	i32 line_count = 0;
+    i32 line_count = 0;
 
     while (getline(&line, &len, fp) != -1) {
         u8 *c_int = line;
-		i32 start = 0;
-		i32 end = 0;
-		i32 prev_end = 0;
-		i32 num = 0;
-		while ((num = str_next_i32_0(&c_int, &start, &end)) != -1) {
-			vector_push(&numbers, (&(struct number_t){num, start + prev_end + 1, end + prev_end + 1, line_count }));
-			prev_end += end + 1;
-		}
+        i32 start = 0;
+        i32 end = 0;
+        i32 prev_end = 0;
+        i32 num = 0;
+        while ((num = str_next_i32_0(&c_int, &start, &end)) != -1) {
+            vector_push(&numbers, (&(struct number_t){ num, start + prev_end + 1,
+                                                       end + prev_end + 1, line_count }));
+            prev_end += end + 1;
+        }
 
-		u8 *c_sym = line;
-		for (i32 i = 1; *c_sym != '\0' && *c_sym != '\n'; c_sym++, i++) {
-			if (!isdigit(*c_sym) && *c_sym != '.') {
-				vector_push(&symbols, &((struct symbol_t){ *c_sym, i, line_count }));
-			}
-		}
+        u8 *c_sym = line;
+        for (i32 i = 1; *c_sym != '\0' && *c_sym != '\n'; c_sym++, i++) {
+            if (!isdigit(*c_sym) && *c_sym != '.') {
+                vector_push(&symbols, &((struct symbol_t){ *c_sym, i, line_count }));
+            }
+        }
 
-		line_count++;
+        line_count++;
     }
 
-	i64 sum = 0;
+    i64 sum = 0;
 
-	vector_foreach(&numbers, lambda(void, (const struct number_t *const num), {
-		for (i32 i = 0; i < vector_get_size(&symbols); i++) {
-			struct symbol_t sym = vector_get(&symbols, i);
-			if (sym.line == num->line) {
-				if (sym.pos == num->start - 1 || sym.pos == num->end + 1) {
-					sum += num->num;
-					break;
-				}
-			} else if (sym.line == num->line - 1 || sym.line == num->line + 1) {
-				bool elem_within_left_range = num->start <= sym.pos && num->end >= sym.pos - 1;
-				bool elem_within_right_range = num->start <= sym.pos + 1 && num->end >= sym.pos;
-				if (elem_within_left_range || elem_within_right_range) {
-					sum += num->num;
-					break;
-				}
-			}
-		}
-	}));
+    vector_foreach(&numbers, lambda(void, (const struct number_t *const num), {
+        for (i32 i = 0; i < vector_get_size(&symbols); i++) {
+            struct symbol_t sym = vector_get(&symbols, i);
+            if (sym.line == num->line) {
+                if (sym.pos == num->start - 1 || sym.pos == num->end + 1) {
+                    sum += num->num;
+                    break;
+                }
+            } else if (sym.line == num->line - 1 || sym.line == num->line + 1) {
+                bool elem_within_left_range = num->start <= sym.pos && num->end >= sym.pos - 1;
+                bool elem_within_right_range = num->start <= sym.pos + 1 && num->end >= sym.pos;
+                if (elem_within_left_range || elem_within_right_range) {
+                    sum += num->num;
+                    break;
+                }
+            }
+        }
+    }));
 
     fprintf(stdout, "Sum: %ld\n", sum);
 
-	free(line);
+    free(line);
 
     fclose(fp);
 
